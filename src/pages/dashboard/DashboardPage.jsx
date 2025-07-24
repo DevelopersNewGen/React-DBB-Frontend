297a1633889c450e4af6276d8f3bbdefde3f0686
shaviher <javiherdeza@gmail.com>
2025-07-24 06:32:43 -0600

    feat: se agrego video

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -5,7 +5,27 @@ export const DashboardPage = () => {
   return (
     <>
       <ResponsiveAppBar />
-      <div>Dashboard</div>
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        style={{
+          position: "fixed",
+          top: "64px", // ajusta según la altura de tu navbar
+          left: 0,
+          width: "100%",
+          height: `calc(100vh - 64px)`, // ocupa el alto total menos el navbar
+          objectFit: "cover",
+          zIndex: -1, // para que no tape el navbar ni otros contenidos
+        }}
+      >
+        <source
+          src="https://res.cloudinary.com/daherc5uz/video/upload/v1753313889/Informe_de_proyecto_1_san5sn.mp4"
+          type="video/mp4"
+        />
+        Tu navegador no soporta la etiqueta de video.
+      </video>
     </>
   );
 };
